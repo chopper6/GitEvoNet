47f9888fdd3f9e950b5cce639442693164e1eacb
chopper6 <corbin.hopper@mail.mcgill.ca>
2017-01-13 20:20:49 -0400

    accelerating pressurize()

--- lib/solver.py
+++ lib/solver.py
@@ -7,76 +7,35 @@ def solve_knapsack (kp_instance, knapsack_solver):
     assert (N==len(B_dict)==len(D_dict))
     if N == 0:
         return []
-    else:        
-        grey_genes   = [key for key in B_dict.keys() if (B_dict[key]>0 and D_dict[key] > 0) ]
-        green_genes  = [key for key in B_dict.keys() if (B_dict[key]>0 and D_dict[key]==0) ]
-        red_genes  = [key for key in B_dict.keys() if B_dict[key]==0]
+    else:
+        GENES_in, grey_genes, green_genes, red_genes = [], [], [], []
+        G, B, D = [],[],[]
+        #attempt to compress, compiler might already handle though
+        i=0
+        for key in B_dict.keys():
+            if (B_dict[key]>0 and D_dict[key] > 0):
+                grey_genes.append(key)
+                G[i], B[i], D[i] = key, (c_int)(B_dict[key]), (c_int)(D_dict[key])
+                i += 1
+            elif (B_dict[key]>0 and D_dict[key]==0):
+                green_genes.append(key)
+                GENES_in.append((key, B_dict[key], D_dict[key]))
+            elif (B_dict[key]==0):
+                red_genes.append(key)
+
         assert (len(grey_genes)+len(green_genes)+len(red_genes)) == len (B_dict.keys())
-        N = len (grey_genes)      
-        
-        G, B, D, F, solver_returns, i = ['' for x in range(0,N)], (c_int*N)(),  (c_int*N)(), (c_int*N)(),  (c_int*4)(), 0
-        for key in grey_genes:            
-            G[i], B[i], D[i], i = key, B_dict[key], D_dict[key], i+1
+        N = len (grey_genes)
+        F, solver_returns = (c_int*N)(),  (c_int*4)()
         #---------------------------------------------------------------------    
         knapsack_solver.solve (B, D, T_edges, N, F, solver_returns)   # WARNING: minknap.so does not return the correct knapsack weight (solver_returns[1])                          
         #---------------------------------------------------------------------
-        TOTAL_Bin, TOTAL_Din, TOTAL_Bout, TOTAL_Dout, GENES_in, GENES_out = 0, 0, 0, 0, [], []
+        #old: TOTAL_Bin, TOTAL_Din, TOTAL_Bout, TOTAL_Dout, GENES_in, GENES_out = 0, 0, 0, 0, [], []
+        GENES_in = []
         for g in range (0, N):
-            assert (F[g] == 0) or (F[g] == 1) #TODO: remove assert
-            if F[g] == 1:                               
-                TOTAL_Bin += B[g]
-                TOTAL_Din += D[g]
-                GENES_in.append ((G[g], B[g], D[g]))  
-            else:
-                TOTAL_Bout += B[g]
-                TOTAL_Dout += D[g]
-                GENES_out.append ((G[g], B[g], D[g]))
+            if F[g] == 1:
+                GENES_in.append ((G[g], B[g], D[g]))
 
-        #assert (TOTAL_Bin == solver_returns [0]) and (TOTAL_Din == solver_returns [1]) and ((len(GENES_out)+len(GENES_in)) == len(G) == len (F) == N) #TODO: remove assert
-        assert (TOTAL_Bin == solver_returns [0]) and ((len(GENES_out)+len(GENES_in)) == len(G) == len (F) == N)
-
-        for key in green_genes: # green_genes are automatically in knapsack
-            TOTAL_Bin += B_dict[key]
-            GENES_in.append((key, B_dict[key], D_dict[key]))
-        for key in red_genes: # red_genes are automatically outside knapsack
-            TOTAL_Dout += D_dict[key]
-            GENES_out.append((key, B_dict[key], D_dict[key]))
-      
         coresize  =  solver_returns[2] #if DP_solver.so is used,  coresize = len (grey_genes)      
         execution_time = solver_returns[3]
-        return [TOTAL_Bin, TOTAL_Din, TOTAL_Bout, TOTAL_Dout, GENES_in, GENES_out, green_genes, red_genes, grey_genes, coresize, execution_time]
-
-
+        return [GENES_in, len(green_genes), len(red_genes), len(grey_genes)]
 # --------------------------------------------------------------------------------------------------
-def solve_knapsack_heuristic(kp_instance):
-    # kp_instance is a tuple: (  {"gene":benefit},  {"gene":damage},  knapsack_size(=tolerance)   )
-    B_dict, D_dict, T_edges, N = kp_instance[0][0], kp_instance[0][1], kp_instance[0][2], len(kp_instance[0][0].keys())
-
-    assert (N == len(B_dict) == len(D_dict))
-    if N == 0:
-        return []
-    else:
-        grey_genes = [key for key in B_dict.keys() if (B_dict[key] > 0 and D_dict[key] > 0)]
-        green_genes = [key for key in B_dict.keys() if (B_dict[key] > 0 and D_dict[key] == 0)]
-        red_genes = [key for key in B_dict.keys() if B_dict[key] == 0]
-        assert (len(grey_genes) + len(green_genes) + len(red_genes)) == len(B_dict.keys())
-
-        num_green = len(green_genes)
-        num_red = len(red_genes)
-        num_grey = len(grey_genes)
-
-        N = num_grey + num_red + num_green
-
-        G, B, D, i = ['' for x in range(0, N)], [None for j in range (N)], [None for j in range (N)], 0
-        for key in grey_genes:
-            G[i], B[i], D[i], i = key, B_dict[key], D_dict[key], i + 1
-        i=num_grey
-        for key in green_genes:
-            G[i], B[i], D[i], i = key, B_dict[key], D_dict[key], i + 1
-        i=num_grey+num_green
-        for key in red_genes:
-            G[i], B[i], D[i], i = key, B_dict[key], D_dict[key], i + 1
-
-
-
-        return G, B, D, num_green, num_red, num_grey
