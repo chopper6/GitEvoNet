ef3458bebfc98424044ad37ad0b75cfa05a6113a
chopper6 <corbin.hopper@mail.mcgill.ca>
2017-03-31 20:37:39 -0300

    degree fitness plot

--- lib/analysis/slice_plots.py
+++ lib/analysis/slice_plots.py
@@ -6,6 +6,30 @@ import numpy as np
 #TODO: all of it
 
 def leaf_fitness(dirr, Pr, BD_leaf_fitness):
+    maxBD = len(Pr)
+    assert(maxBD == len(Pr[0]))
+
+    Pr_fitness = [0 for i in range(maxBD)]
+
+    for B in range(maxBD):
+        for D in range (maxBD):
+            if (B+D < maxBD): Pr_fitness[B+D] += Pr[B][D]*BD_leaf_fitness[B][D]
+
+    index = [i for i in range(maxBD)]
+
+    plt.bar(index, Pr_fitness)
+
+    ax = plt.gca()
+
+    ax.set_ylabel("Cumulative Fitness", fontsize=12)
+    ax.set_xlabel("Degree", fontsize=12)
+    plt.title("Degree Fitness by BD Probability", fontsize=15)
+
+    plt.savefig(dirr + "Degree_Fitness.png")
+    plt.clf()
+    plt.cla()
+    plt.close()
+
     return
 
 
