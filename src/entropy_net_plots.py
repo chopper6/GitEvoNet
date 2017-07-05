0704afe7f2b90f67d276e12dc96df29eade72fd0
chopper6 <corbin.hopper@mail.mcgill.ca>
2017-07-05 09:49:22 -0400

    compariosn

--- src/entropy_net_plots.py
+++ src/entropy_net_plots.py
@@ -96,11 +96,100 @@ def undir_deg_distrib(net_file, destin_path, title, biased, bias_on):
         plt.close()
 
 
+def comparison_undir_deg_distrib(net_file1, net_file2, destin_path, title, biased, bias_on):
+    print('undir_deg_distrib: biased=' + str(biased) + ', bias_on=' + str(bias_on))
+    #update_rcParams()
+    # each line in 'input.txt' should be: [network name (spaces allowed) followed by /path/to/edge/file.txt/or/pickled/network.dump]
+
+    net1 = nx.read_edgelist(net_file1, nodetype=int, create_using=nx.DiGraph())
+    net2 = nx.read_edgelist(net_file2, nodetype=int, create_using=nx.DiGraph())
+    nets = [net1, net2]
+    colors = ['#0099cc','#ff5050', '#6699ff']
+
+    k=0
+    for net in nets:
+
+        color_choice = colors[k]
+        H = []
+        #loglog
+        degrees = list(net.degree().values())
+        degs, freqs = np.unique(degrees, return_counts=True)
+        tot = float(sum(freqs))
+        freqs = [(f/tot)*100 for f in freqs]
+
+
+
+        #derive vals from conservation scores
+        consv_vals = []
+        if (biased == True or biased == 'True'):
+            print('using bias')
+            for deg in degs: #deg consv is normalized by num nodes
+                avg_consv, num_nodes = 0,0
+                for node in net.nodes():
+                    if (net.degree(node) == deg):
+                        if (bias_on == 'nodes'):
+                            avg_consv += net.node[node]['conservation_score']
+                        elif (bias_on == 'edges'): #node consv is normalized by num edges
+                            node_consv, num_edges = 0, 0
+                            for edge in net.edges(node):
+                                node_consv += net[edge[0]][edge[1]]['conservation_score']
+                                num_edges += 1
+                            if (num_edges != 0): node_consv /= num_edges
+                        num_nodes += 1
+                avg_consv /= num_nodes
+                consv_vals.append(avg_consv)
+            assert(len(consv_vals) == len(degs))
+            cmap = plt.get_cmap('plasma')
+            consv_colors = cmap(consv_vals)
+
+            plt.loglog(degs, freqs, basex=10, basey=10, linestyle='',  linewidth=2, c = consv_colors, alpha=1, markersize=8, marker='D', markeredgecolor='None')
+
+        else:
+            plt.loglog(degs, freqs, basex=10, basey=10, linestyle='',  linewidth=2, color = color_choice, alpha=1, markersize=8, marker='D', markeredgecolor='None')
+
+        patch =  mpatches.Patch(color=color_choice, label=title + "_" + type)
+        H = H + [patch]
+        k+=1
+
+        #FORMAT PLOT
+        ax = plt.gca() # gca = get current axes instance
+
+        ax.set_xlim([0,1000])
+        ax.set_ylim([0,1000])
+
+        ax.spines["top"].set_visible(False)
+        ax.spines["right"].set_visible(False)
+        plt.tick_params(axis='both', which='both', right='off', top='off') #http://matplotlib.org/api/axes_api.html#matplotlib.axes.Axes.tick_params
+        plt.legend(loc='upper right', handles=H, frameon=False,fontsize= 11)
+        plt.xlabel('Degree')
+        plt.ylabel('Number of Nodes with Given Degree')
+        #plt.title('Degree Distribution of ' + str(title) + ' vs Simulation')
+
+        plt.tight_layout()
+        plt.savefig(destin_path + title + "_" + type + ".png", dpi=300,bbox='tight') # http://matplotlib.org/api/figure_api.html#matplotlib.figure.Figure.savefig
+        plt.clf()
+        plt.cla()
+        plt.close()
+
+
 
 
 if __name__ == "__main__":
     base_dir = "/home/2014/choppe1/Documents/EvoNet/virt_workspace/data/output/entropy2_again/" #customize for curr work
     real_net_file = "/home/2014/choppe1/Documents/EvoNet/virt_workspace/data/input/input_all_nets.txt" #check this is still on yamaska
+
+    if sys.argv[1] == 'comparison':
+        net1_path = sys.argv[2]
+        net2_path = sys.argv[3]
+        biased = False  # sys.argv[2]
+        bias_on = None  # sys.argv[3]
+
+        if not os.path.exists(net1_path + "/undirected_degree_distribution/"):
+            os.makedirs(net1_path + "/undirected_degree_distribution/")
+
+        comparison_undir_deg_distrib(base_dir+ "/" + net1_path, base_dir + "/" + net2_path, net1_path + "/undirected_degree_distribution/", "comparison", biased, bias_on)
+
+
 
     biased = False #sys.argv[2]
     bias_on = None #sys.argv[3]
