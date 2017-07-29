92a682d2b9cc651102f805f596ec15b62b021ca7
chopper6 <corbin.hopper@mail.mcgill.ca>
2017-07-28 21:03:17 -0400

    .

--- src/plot_vs_real.py
+++ src/plot_vs_real.py
@@ -18,7 +18,7 @@ def plot_pairs(real_net_file, real_net_name, sim_net_file, plot_title):
         name, network_file = line.strip().split(' ')
         if (name==real_net_name or real_net_name == 'all'):
             color_choice = colors[i]
-            xmax = 200
+            xmin, xmax = .5, 200
             if (re.match(re.compile("[a-zA-Z0-9]*PPI"), name)):
                 color_choice = colors[0]
                 name = name.replace('PPI', ' PPI')
@@ -30,16 +30,16 @@ def plot_pairs(real_net_file, real_net_name, sim_net_file, plot_title):
                 name = name.replace('PQ', ' PSICQUIC')
             elif (re.match(re.compile("[a-zA-Z0-9]*EN-[a-zA-Z0-9]*"), name)):
                 color_choice = colors[3]
-                xmax = 500
+                xmin, xmax = .02, 500
                 name = name.replace('EN-', ' ENCODE-')
             elif (re.match(re.compile("[a-zA-Z0-9]*Liu"), name)):
                 color_choice = colors[4]
                 name = name.replace('Liu', ' Liu')
-                xmax = 850
+                xmin, xmax = .02, 850
             elif (re.match(re.compile("[a-zA-Z0-9]*TRRUST"), name)):
                 color_choice = colors[5]
                 name = name.replace('TRRUST', ' TRRUST')
-                xmax = 500
+                xmin, xmax = .02, 500
             else:
                 color_choice = colors[6]
 
@@ -100,7 +100,7 @@ def plot_pairs(real_net_file, real_net_name, sim_net_file, plot_title):
             # ax.set_yscale('log')
             #ax.set_xlim([0.7, 200]) #TODO: change these?
             #ax.set_ylim([.1, 100])
-            ax.set_xlim([.5,int(xmax)])
+            ax.set_xlim([xmin,xmax])
             ax.set_ylim([.02,100])
 
             xfmatter = ticker.FuncFormatter(LogXformatter)
