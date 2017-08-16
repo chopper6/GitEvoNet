51f62319d324040f7ce27eba0743b81fabbf5b7b
chopper6 <corbin.hopper@mail.mcgill.ca>
2017-08-16 13:36:38 -0400

    .

--- lib/reducer.py
+++ lib/reducer.py
@@ -47,7 +47,7 @@ def exp_BDs(net, configs):
     BD_criteria = configs['BD_criteria']
     assert (BD_criteria == 'both') #temp
 
-    pressure = float(configs['pressure']/float(100))
+    pressure = float(float(configs['pressure'])/float(100))
     pressure_relative = int(len(net.edges())*pressure)
     edges = net.edges()
     rd.shuffle(edges)
