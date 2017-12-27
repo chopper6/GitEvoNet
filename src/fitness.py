a76f75e5dde215810d39455b0fb1719f8b191946
chopper6 <corbin.hopper@mail.mcgill.ca>
2017-12-27 11:35:44 -0500

    info norm

--- src/fitness.py
+++ src/fitness.py
@@ -21,6 +21,10 @@ def node_fitness(net, leaf_metric):
         #if (B+D == 0): print ("WARNING fitness.node_fitness(): B+D == 0")
         net.node[n]['fitness'] += leaf_fitness.node_score(leaf_metric, B,D)
         #print(net[node]['fitness'])
+        if (leaf_metric=='info'):
+            info = net.node[n]['fitness']
+            num_edges = len(net.in_edges(n) + net.out_edges(n))
+            net.node[n]['fitness'] = math.pow(2*info,num_edges)
 
 def node_product(net):
     #now uses log-likelihood estimation: product --> sum log
